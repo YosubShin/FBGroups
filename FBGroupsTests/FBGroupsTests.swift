b1cf4e6af3f6c9b6099989b0149faa47544a4099
Yosub Shin <yosub@yosubshin.com>
2014-06-28 12:01:45 -0500

    Initial Commit

---
+++ FBGroupsTests/FBGroupsTests.swift
@@ -0,0 +1,35 @@
+//
+//  FBGroupsTests.swift
+//  FBGroupsTests
+//
+//  Created by Yosub Shin on 6/28/14.
+//  Copyright (c) 2014 yosubshin. All rights reserved.
+//
+
+import XCTest
+
+class FBGroupsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
